1e5172b2866d39ea6dc7015ffb21f0b488a861fb
fnbikorea <fnbikorea@fnbikorea-PC>
2018-08-14 17:47:00 +0900

    12341111

--- src/main/java/board_dev/sample/controller/SampleController.java
+++ src/main/java/board_dev/sample/controller/SampleController.java
@@ -1,6 +1,9 @@
 package board_dev.sample.controller;
 
+import java.util.List;
 import java.util.Map;
+
+import javax.annotation.Resource;
 
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
@@ -11,14 +14,17 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class SampleController {
 	Logger log = Logger.getLogger(this.getClass());
-//	protected static Logger log = Logger.getLogger(LoggerInterceptor.class.getName());
 
+	@Resource(name="sampleService")
+	private SampleService sampleService;
 	
-	
-	@RequestMapping(value="/sample/openSampleList.do")
-	public ModelAndView openSampleList(Map<String, Object> commandMap) throws Exception {
-		ModelAndView mv = new ModelAndView("");
-		log.debug("인터셉트 테스트");
+	@RequestMapping(value="/sample/openSampleBoardList.do")
+	public ModelAndView openSampleBoardList(Map<String, Object> commandMap) throws Exception {
+		ModelAndView mv = new ModelAndView("/sample/boardList");
+		
+		List<Map<String, Object>> list = sampleService.selectBoardList(commandMap);
+		mv.addObject("list", list);
+		
 		return mv;
 	}
 }
