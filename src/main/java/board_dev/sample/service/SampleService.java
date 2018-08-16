27dd33f00f41e0d780968bd4e3ce8df0b07573cc
fnbikorea <fnbikorea@fnbikorea-PC>
2018-08-16 17:56:54 +0900

    123123123

---
+++ src/main/java/board_dev/sample/service/SampleService.java
@@ -0,0 +1,11 @@
+package board_dev.sample.service;
+
+import java.util.List;
+import java.util.Map;
+
+public interface SampleService {
+
+	List<Map<String, Object>> selectBoardList(Map<String, Object> map) throws Exception;
+	
+	void insetBoard(Map<String, Object> map) throws Exception;
+}
